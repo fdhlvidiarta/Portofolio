caf5f96e0d4ca87ba07fb405ea6e70189ccda8f8
fadhil <fadhilv05@gmail.com>
2026-08-19 00:00:01 +0700

    update

--- assets/script.js
+++ assets/script.js
@@ -335,3 +335,21 @@ window.addEventListener("DOMContentLoaded", () => {
     );
   });
 });
+
+// --- BACK TO TOP LOGIC ---
+const backToTopBtn = document.getElementById("backToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.classList.add("show");
+  } else {
+    backToTopBtn.classList.remove("show");
+  }
+});
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
